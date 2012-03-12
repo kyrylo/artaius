b8642a706d0b331346dce656d5ce8c1febd7baaa
Kyrylo Silin <kyrylosilin@gmail.com>
2012-03-12 19:28:15 +0200

    Save new users to database, so we don't need to query kag2d.com every time

--- lib/artaius/plugins/autovoice_premiums.rb
+++ lib/artaius/plugins/autovoice_premiums.rb
@@ -1,35 +1,59 @@
 module Artaius
   module Plugin
     ###
-    # With help of PremiumAutovoices plugin, Artaius gives a voice for everyone,
+    # With help of AutovoicePremiums plugin, Artaius gives a voice for everyone,
     # who has a premium account in King Arthur's Gold and has authenticated on
     # QuakeNet. Note, that current detection of premium accounts leaves much to
     # be desired, since it relies on HTML parsing. It is necessary measure,
     # because currently KAG has no any kind of API.
-    class PremiumAutovoices
+    class AutovoicePremiums
       include Cinch::Plugin
 
       listen_to :join
 
-      # Listens to joins and gives a voice, if the joined user owns premium.
+      # KAG's official website with appended 'user' for the sake of convenience.
+      KAG_WEBSITE = 'http://www.kag2d.com/user/'
+
+      # Listens to joins and gives a voice to
+      # incomer, if they have a premium account.
       def listen(m)
+        incomer = m.user
         # Defy unauthenticated players.
-        if m.user.authed? && m.user.nick != bot.nick
-          m.channel.voice(m.user) if premium?(m.user.authname)
+        if incomer.authed? && incomer.nick != bot.nick
+          m.channel.voice(incomer) if premium?(incomer)
         end
       end
 
       protected
 
-      # Detects account type of the given nickname. Returns true, if the
-      # nickname is registered and it is premium. Otherwise, returns false.
-      def premium?(nickname)
-        doc = Nokogiri::HTML open("http://www.kag2d.com/user/#{nickname}/")
-        if doc.css(".info ul li strong").children[0].text == "Premium account"
-          @premium_account = true
-        end
-      rescue
-        false
+      # Creates a new record, if player doesn't exist in database.
+      # Returns true, if the the player has premium account.
+      def premium?(player)
+        create_new(player) unless Player.exists?(player)
+        Player.premium?(player)
+      end
+
+      # Creates a new player in database.
+      def create_new(player)
+        kag_player = find_kag_player(player)
+        Player.create(:irc_authname => player.authname,
+                      :kag_name     => kag_player[:kag_name],
+                      :premium      => kag_player[:premium])
+      end
+
+      # Finds requisites of the player at KAG's website. Returns a
+      # hash with the player's nickname in KAG and account type.
+      def find_kag_player(player)
+        http_name = player.authname.gsub(/[^0-9\w]/, '')
+        doc = Nokogiri::HTML open(KAG_WEBSITE + http_name.downcase)
+
+        kag_name = doc.css('#profile .info h1').text.strip
+        account  = doc.css('#profile .info ul li strong').children[0].text
+        premium  = account == 'Premium account'
+
+        return { :kag_name => kag_name, :premium => premium }
+      rescue OpenURI::HTTPError
+        puts "There is no KAG player with such a nickname: #{player.authname}"
       end
 
     end
