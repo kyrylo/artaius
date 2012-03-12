6cd48075a29f0c02e13fc2804ccba816c20b37bd
Kyrylo Silin <kyrylosilin@gmail.com>
2012-03-12 18:50:16 +0200

    Add Player#exists? and Player#premium? for convenience purposes

--- lib/artaius/models/player.rb
+++ lib/artaius/models/player.rb
@@ -1,5 +1,16 @@
 module Artaius
   class Player < Sequel::Model(:players)
     plugin :timestamps, :update_on_create => true
+
+    # Returns true, if the player exists in database.
+    def self.exists?(player)
+      !self[:irc_authname => player.authname].nil?
+    end
+
+    # Returns true, if the player has premium account.
+    def self.premium?(player)
+      self[:irc_authname => player.authname][:premium]
+    end
+
   end
 end
