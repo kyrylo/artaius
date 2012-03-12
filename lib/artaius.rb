6d66007d6665bf3e928dd621f66fa9f2bcb7cf6e
Kyrylo Silin <kyrylosilin@gmail.com>
2012-03-12 18:48:07 +0200

    Require stuff and add VERSION constant

--- lib/artaius.rb
+++ lib/artaius.rb
@@ -1,7 +1,21 @@
 require 'cinch'
+
+require 'sequel'
+require 'sequel/extensions/migration'
+
+require 'open-uri'
+require 'nokogiri'
 require 'yaml'
 
+ENV['DB_PATH'] = File.expand_path('db')
+
 Dir.chdir(File.dirname(__FILE__)) do
-  Dir['artaius/plugins/*.rb'].each { |plugin| require_relative plugin }
   Dir['artaius/*.rb'].each { |file| require_relative file }
+  Dir['artaius/models/*.rb'].each { |model| require_relative model }
+  Dir['artaius/plugins/*.rb'].each { |plugin| require_relative plugin }
+end
+
+module Artaius
+  # The version of Artaius you're using.
+  VERSION = '0.0.0'
 end
