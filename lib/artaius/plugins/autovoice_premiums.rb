d1a91d2b9c0f3ddf02434318287879c69d1083c3
Kyrylo Silin <kyrylosilin@gmail.com>
2012-03-14 03:21:36 +0200

    Separate player finding method to its own module

--- lib/artaius/plugins/autovoice_premiums.rb
+++ lib/artaius/plugins/autovoice_premiums.rb
@@ -8,11 +8,9 @@ module Artaius
     # because currently KAG has no any kind of API.
     class AutovoicePremiums
       include Cinch::Plugin
+      include Artaius::KAGPlayerFinder
 
       listen_to :join
-
-      # KAG's official website with appended 'user' for the sake of convenience.
-      KAG_WEBSITE = 'http://www.kag2d.com/user/'
 
       # Listens to joins and gives a voice to
       # incomer, if they have a premium account.
@@ -29,8 +27,9 @@ module Artaius
       # Creates a new record, if player doesn't exist in database.
       # Returns true, if the the player has premium account.
       def premium?(player)
-        create_new(player) unless Player.exists?(player)
-        Player.premium?(player)
+        irc_authname = { :irc_authname => player.authname }
+        create_new(player) unless Player.exists?(irc_authname)
+        Player.premium?(irc_authname)
       end
 
       # Creates a new player in database.
@@ -39,21 +38,6 @@ module Artaius
         Player.create(:irc_authname => player.authname,
                       :kag_name     => kag_player[:kag_name],
                       :premium      => kag_player[:premium])
-      end
-
-      # Finds requisites of the player at KAG's website. Returns a
-      # hash with the player's nickname in KAG and account type.
-      def find_kag_player(player)
-        http_name = player.authname.gsub(/[^0-9\w]/, '')
-        doc = Nokogiri::HTML open(KAG_WEBSITE + http_name.downcase)
-
-        kag_name = doc.css('#profile .info h1').text.strip
-        account  = doc.css('#profile .info ul li strong').children[0].text
-        premium  = account == 'Premium account'
-
-        return { :kag_name => kag_name, :premium => premium }
-      rescue OpenURI::HTTPError
-        puts "There is no KAG player with such a nickname: #{player.authname}"
       end
 
     end
