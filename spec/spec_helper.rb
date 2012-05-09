ba997301e6199a497ca924591c0fd91aa855d6d8
Kyrylo Silin <kyrylosilin@gmail.com>
2012-05-10 01:47:28 +0300

    Fix wrong locale reload path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,5 @@ require 'fivemat/minitest/autorun'
 require_relative '../lib/artaius'
 
 # Force R18n to use English locale for testing.
-Artaius.const_set("I18n", R18n.from_env('config/load_file', :en))
+Artaius.const_set("I18n", R18n.from_env('config/locales', :en))
 Dir['lib/artaius/plugins/*.rb'].each { |plugin| load plugin }
